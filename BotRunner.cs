2ff98ede830993b5f7d3f73ce6c009be2526f59a
rime <s1337228@yandex.ru>
2022-11-20 15:27:08 +0300

    role give/take using reactions

---
+++ BotRunner.cs
@@ -0,0 +1,100 @@
+﻿using Discord;
+using Discord.WebSocket;
+using myGreeterBot.Data;
+
+namespace myGreeterBot;
+
+public class BotRunner
+{
+
+    private readonly IServiceProvider _serviceProvider;
+    
+    public BotRunner()
+    {
+        _serviceProvider = CreateProvider();
+    }
+
+    private IServiceProvider CreateProvider()
+    {
+        var config = new DiscordSocketConfig()
+        {
+            GatewayIntents = GatewayIntents.All
+        };
+
+        var collection = new ServiceCollection()
+            .AddSingleton(config)
+            .AddSingleton<DiscordSocketClient>();
+
+        return collection.BuildServiceProvider();
+    }
+
+    private DiscordSocketClient _client;
+
+    public async Task Run()
+    {
+        _client = _serviceProvider.GetRequiredService<DiscordSocketClient>();
+
+        _client.Log += Log;
+
+        // _client.Ready += OnClientReady;
+
+        _client.ReactionAdded += GetReactionMethod(true);
+        _client.ReactionRemoved += GetReactionMethod(false);
+        
+        string token = new Settings().GetSettings().token;
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        
+        await Task.Delay(-1);
+    }
+
+    private readonly Dictionary<string, ulong> emoteAndRole = new()
+    {
+        {"🤔", 1044049914965012520},
+        {"🖕", 1044049961416917022}
+    };
+
+    // private Task OnReactionRemoved(Cacheable<IUserMessage, ulong> useMsg, Cacheable<IMessageChannel, ulong> msgChannel, SocketReaction socketReaction)
+    // {
+    //     var emoteName = socketReaction.Emote.Name;
+    //
+    //     var user = socketReaction.User.Value as IGuildUser;
+    //     user.RemoveRoleAsync(emoteAndRole[emoteName]);
+    //     
+    //     return Task.CompletedTask;
+    // }
+    //
+    // private Task OnReactionAdded(Cacheable<IUserMessage, ulong> userMsg, Cacheable<IMessageChannel, ulong> msgChannel, SocketReaction socketReaction)
+    // {
+    //     var emoteName = socketReaction.Emote.Name;
+    //
+    //     var user = socketReaction.User.Value as IGuildUser;
+    //     user.AddRoleAsync(emoteAndRole[emoteName]);
+    //
+    //     return Task.CompletedTask;
+    // }
+
+    private Func<Cacheable<IUserMessage, ulong>, Cacheable<IMessageChannel, ulong>, SocketReaction, Task> GetReactionMethod(bool isAdded) 
+        => (_, _, socketReaction) => 
+        {
+            var emoteName = socketReaction.Emote.Name;
+            var user = socketReaction.User.Value as IGuildUser;
+    
+            if (isAdded)
+                user.AddRoleAsync(emoteAndRole[emoteName]);
+            else
+                user.RemoveRoleAsync(emoteAndRole[emoteName]);
+
+            return Task.CompletedTask; 
+        };
+
+    // private async Task OnClientReady()
+    // {
+    // }
+
+    private Task Log(LogMessage log)
+    {
+        Console.WriteLine(log);
+        return Task.CompletedTask;
+    }
+}
---
+++ Data/Settings.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace myGreeterBot.Data;
+
+public class Settings
+{
+    public dynamic GetSettings()
+    {
+        string jsonAsString;
+        using (StreamReader streamReader = new StreamReader(Environment.CurrentDirectory + @"\Data\Settings.json")) 
+            jsonAsString = streamReader.ReadToEnd();
+        
+        return JsonConvert.DeserializeObject<dynamic>(jsonAsString);
+    }
+}
